74175489bcf4b17165102ae049766283c7a5fab9
damianu <damian@datagentleman.io>
2026-02-07 23:11:47 +0100

    Bitbox encoder/decoder

---
+++ bitbox.go
@@ -0,0 +1,112 @@
+package bitbox
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// Encode objects
+func Encode(objects ...any) *Buffer {
+	buf := NewBuffer([]byte{})
+
+	for _, obj := range objects {
+		val := reflect.ValueOf(obj)
+		val = reflect.Indirect(val) // indirect pointers
+
+		if !val.IsValid() {
+			continue // skip nil pointers
+		}
+
+		// Encode []byte
+		if IsByteList(val) {
+			l := uint32(len(val.Bytes()))
+
+			buf.data = append(buf.data, BytesPtr(&l)...) // length prefix
+			buf.data = append(buf.data, val.Bytes()...)  // bytes
+			continue
+		}
+
+		// Encode string
+		if val.Kind() == reflect.String {
+			s := val.String()
+			l := uint32(len(s))
+
+			buf.data = append(buf.data, BytesPtr(&l)...) // length prefix
+			buf.data = append(buf.data, s...)            // string bytes
+			continue
+		}
+
+		// Encode basic types
+		buf.data = append(buf.data, bytesPtr(val)...)
+		continue
+	}
+
+	return buf
+}
+
+// Decode objects
+func Decode(buf *Buffer, objects ...any) {
+	for _, obj := range objects {
+		val := reflect.ValueOf(obj)
+		if !val.IsValid() || val.Kind() != reflect.Pointer || val.IsNil() {
+			continue
+		}
+
+		val = val.Elem() // use value instead of pointer
+
+		switch {
+		case IsByteList(val):
+			l := uint32(0)
+			buf.Decode(&l)
+			val.SetBytes(append(val.Bytes(), buf.Take(int(l))...))
+		case val.Kind() == reflect.String:
+			l := uint32(0)
+			buf.Decode(&l)
+			val.SetString(string(buf.Take(int(l))))
+		default:
+			// Decode fixed-size basic types (including aliases) and structs.
+			// Structs must be memory aligned.
+			buf.Copy(bytesPtr(val))
+		}
+	}
+}
+
+// Get pointer to fixed type (including structs) and cast it to []byte.
+// When passing structs, make sure they are memory aligned otherwise
+// you would have extra bytes.
+func BytesPtr[T any](obj *T) []byte {
+	size := unsafe.Sizeof(*obj)
+	return unsafe.Slice((*byte)(unsafe.Pointer(obj)), size)
+}
+
+// Get pointer from reflect.Value and cast it to []byte.
+// Value must be addressable.
+func bytesPtr(val reflect.Value) []byte {
+	if !val.CanAddr() {
+		panic("value is not addressable")
+	}
+
+	ptr := unsafe.Pointer(val.UnsafeAddr())
+	size := val.Type().Size()
+
+	return unsafe.Slice((*byte)(ptr), size)
+}
+
+// Check if we deal with byte slice/array
+func IsByteList(val reflect.Value) bool {
+	if IsSlice(val) || IsArray(val) {
+		return val.Type().Elem().Kind() == reflect.Uint8
+	}
+
+	return false
+}
+
+// Check if we have slice
+func IsSlice(val reflect.Value) bool {
+	return val.Kind() == reflect.Slice
+}
+
+// Check if we have array
+func IsArray(val reflect.Value) bool {
+	return val.Kind() == reflect.Array
+}
---
+++ buffer.go
@@ -0,0 +1,49 @@
+package bitbox
+
+// Handy Buffer class for encoding/decoding data
+type Buffer struct {
+	data []byte
+	off  int
+}
+
+// Create new Buffer
+func NewBuffer(data []byte) *Buffer {
+	return &Buffer{data: data, off: 0}
+}
+
+// Decode data from buffer into objects
+func (b *Buffer) Decode(objects ...any) {
+	Decode(b, objects...)
+}
+
+// Return remaining buffer length
+func (b *Buffer) Len() int {
+	return len(b.data[b.off:])
+}
+
+// Wrapper for copy()
+func (b *Buffer) Copy(dst []byte) int {
+	n := copy(dst, b.data[b.off:])
+	b.off += n
+
+	return n
+}
+
+// Take next N bytes from buffer.
+// This will advance offset.
+func (b *Buffer) Take(num int) []byte {
+	off := b.off
+	b.off += num
+
+	return b.data[off:b.off]
+}
+
+// Return remaining bytes from buffer
+func (b *Buffer) Data() []byte {
+	return b.data[b.off:]
+}
+
+// Advance data offset
+func (b *Buffer) Consume(n int) {
+	b.off += n
+}
