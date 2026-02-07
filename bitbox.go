e6b93d57a49edfbc6a123187d26d79009a9fec92
damianu <damian@datagentleman.io>
2026-02-07 23:56:28 +0100

    Cleaning bytes

--- bitbox.go
+++ bitbox.go
@@ -18,11 +18,11 @@ func Encode(objects ...any) *Buffer {
 		}
 
 		// Encode []byte
-		if IsByteList(val) {
+		if IsBytes(val) {
 			l := uint32(len(val.Bytes()))
 
-			buf.data = append(buf.data, BytesPtr(&l)...) // length prefix
-			buf.data = append(buf.data, val.Bytes()...)  // bytes
+			buf.data = append(buf.data, ToBytes(&l)...) // length prefix
+			buf.data = append(buf.data, val.Bytes()...) // bytes
 			continue
 		}
 
@@ -31,13 +31,13 @@ func Encode(objects ...any) *Buffer {
 			s := val.String()
 			l := uint32(len(s))
 
-			buf.data = append(buf.data, BytesPtr(&l)...) // length prefix
-			buf.data = append(buf.data, s...)            // string bytes
+			buf.data = append(buf.data, ToBytes(&l)...) // length prefix
+			buf.data = append(buf.data, s...)           // string bytes
 			continue
 		}
 
 		// Encode basic types
-		buf.data = append(buf.data, bytesPtr(val)...)
+		buf.data = append(buf.data, toBytes(val)...)
 		continue
 	}
 
@@ -55,10 +55,10 @@ func Decode(buf *Buffer, objects ...any) {
 		val = val.Elem() // use value instead of pointer
 
 		switch {
-		case IsByteList(val):
+		case IsBytes(val):
 			l := uint32(0)
 			buf.Decode(&l)
-			val.SetBytes(append(val.Bytes(), buf.Take(int(l))...))
+			val.SetBytes(buf.Take(int(l)))
 		case val.Kind() == reflect.String:
 			l := uint32(0)
 			buf.Decode(&l)
@@ -66,7 +66,7 @@ func Decode(buf *Buffer, objects ...any) {
 		default:
 			// Decode fixed-size basic types (including aliases) and structs.
 			// Structs must be memory aligned.
-			buf.Copy(bytesPtr(val))
+			buf.Copy(toBytes(val))
 		}
 	}
 }
@@ -74,14 +74,14 @@ func Decode(buf *Buffer, objects ...any) {
 // Get pointer to fixed type (including structs) and cast it to []byte.
 // When passing structs, make sure they are memory aligned otherwise
 // you would have extra bytes.
-func BytesPtr[T any](obj *T) []byte {
+func ToBytes[T any](obj *T) []byte {
 	size := unsafe.Sizeof(*obj)
 	return unsafe.Slice((*byte)(unsafe.Pointer(obj)), size)
 }
 
 // Get pointer from reflect.Value and cast it to []byte.
 // Value must be addressable.
-func bytesPtr(val reflect.Value) []byte {
+func toBytes(val reflect.Value) []byte {
 	if !val.CanAddr() {
 		panic("value is not addressable")
 	}
@@ -93,7 +93,7 @@ func bytesPtr(val reflect.Value) []byte {
 }
 
 // Check if we deal with byte slice/array
-func IsByteList(val reflect.Value) bool {
+func IsBytes(val reflect.Value) bool {
 	if IsSlice(val) || IsArray(val) {
 		return val.Type().Elem().Kind() == reflect.Uint8
 	}
